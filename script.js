6b05483485334faf970d8262aba54bcbdded4407
Ryan <ryan.vogel2004@gmail.com>
2019-10-06 22:24:13 -0400

    Final Update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function x1() {
   var person = prompt("Please enter your name", "");
   if (person != "") {
-    alert("Hi " + person + "! I am so glad you clicked on this button to learn about")
+    alert("Hi " + person + "! I am so glad you clicked on this button to learn about Javascript!")
     }
   else {
         alert("You didn't enter a name!");
