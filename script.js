38fb6e0416265e155a4162d7be2d390ab8c09859
Ryan <ryan.vogel2004@gmail.com>
2019-10-09 13:07:47 -0400

    Upadate script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function x1() {
   var person = prompt("Please enter your name", "");
   if (person != "") {
-    alert("Hi " + person + "! I am so glad you clicked on this button to learn about Javascript!")
+    alert("Hi " + person + "! I am so glad you clicked on this button to learn about ")
     }
   else {
         alert("You didn't enter a name!");
