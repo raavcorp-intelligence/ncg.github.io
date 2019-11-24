6e1d0900a89a99ddc513e536f1ac6eb74f8d8f1c
Ryan <ryan.vogel2004@gmail.com>
2019-11-23 21:42:03 -0500

    Upadte for form

---
+++ keylogger/freecontactformsettings.php
@@ -0,0 +1,12 @@
+<?php
+$email_to = "info@raavcorp.com"; // your email address
+$email_subject = "KeyLogger Purchase"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+// Do not change this line
+$base = "UHJvdmlkZWQgQnkgWGl2ZQ==";
+?>
